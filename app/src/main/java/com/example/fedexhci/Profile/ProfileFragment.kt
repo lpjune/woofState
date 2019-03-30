41aaf15d1680dd40f4950b094a62a5752d093135
Claudia <claudiaareneee@gmail.com>
2019-03-30 14:38:13 -0400

    WIP started adding popup window

--- app/src/main/java/com/example/fedexhci/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/fedexhci/Profile/ProfileFragment.kt
@@ -1,13 +1,18 @@
 package com.example.fedexhci.Profile
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat.getSystemService
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.transition.Slide
+import android.view.*
+import android.widget.ImageView
+import android.widget.PopupWindow
+import android.widget.Toast
 import com.example.fedexhci.R
 
 class ProfileFragment: Fragment() {
@@ -23,6 +28,56 @@ class ProfileFragment: Fragment() {
         val mProfileRecyclerView = rootView.findViewById(R.id.profile_dogs_recycler_view) as RecyclerView // Add this
         mProfileRecyclerView.layoutManager = GridLayoutManager(activity, 2)
         mProfileRecyclerView.adapter = ProfileRecyclerAdapter()
+
+        val badge1 = rootView.findViewById<ImageView>(R.id.badge1)
+        badge1.setOnClickListener { badgeHasBeenClicked("Badge 1",rootView) }
+
+        val badge2 = rootView.findViewById<ImageView>(R.id.badge2)
+        badge2.setOnClickListener { badgeHasBeenClicked("Badge 2",rootView) }
+
+        val badge3 = rootView.findViewById<ImageView>(R.id.badge3)
+        badge3.setOnClickListener { badgeHasBeenClicked("Badge 3",rootView) }
+
+        val badge4 = rootView.findViewById<ImageView>(R.id.badge4)
+        badge4.setOnClickListener { badgeHasBeenClicked("Badge 4",rootView) }
+
         return rootView
     }
+
+    private fun badgeHasBeenClicked(badgeName: String, view: View){
+        Toast.makeText(activity,"Clicked ".plus(badgeName) ,Toast.LENGTH_LONG).show()
+
+        // Initialize a new layout inflater instance
+        val inflater:LayoutInflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        // Inflate a custom view using layout inflater
+        val view = inflater.inflate(R.layout.badge_popup,null)
+
+
+        var popup: PopupWindow? = null;
+        popup = PopupWindow(view, ConstraintLayout.LayoutParams.MATCH_PARENT,ConstraintLayout.LayoutParams.MATCH_PARENT)
+
+        // Set an elevation for the popup window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popup.elevation = 10.0F
+        }
+
+
+        // If API level 23 or higher then execute the code
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Create a new slide animation for popup window enter transition
+            val slideIn = Slide()
+            slideIn.slideEdge = Gravity.TOP
+            popup.enterTransition = slideIn
+
+            // Slide animation for popup window exit transition
+            val slideOut = Slide()
+            slideOut.slideEdge = Gravity.RIGHT
+            popup.exitTransition = slideOut
+
+        }
+
+
+    }
+
 }
