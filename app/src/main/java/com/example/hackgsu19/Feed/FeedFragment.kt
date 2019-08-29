3061f8b4175cb723961ca8902339a7857fc301ff
Claudia <claudiaareneee@gmail.com>
2019-08-28 22:26:00 -0500

    Started updating colors and added a fab on the feed screen to the map

--- app/src/main/java/com/example/hackgsu19/Feed/FeedFragment.kt
+++ app/src/main/java/com/example/hackgsu19/Feed/FeedFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import com.example.hackgsu19.R
 
 import 	androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
 
 
 class FeedFragment: Fragment() {
@@ -26,11 +28,16 @@ class FeedFragment: Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_feed, container, false)
 
         val mRecyclerView = rootView.findViewById(R.id.manager_recycler_view) as RecyclerView // Add this
-        mRecyclerView.layoutManager = GridLayoutManager(activity, 3)
+        mRecyclerView.layoutManager = GridLayoutManager(activity, 1)
         val myAdapter = FeedRecyclerAdapter()
         myAdapter.setContext(context)
         mRecyclerView.adapter = myAdapter
 
+        val mMapFAB = rootView.findViewById(R.id.floating_action_button_map) as FloatingActionButton
+        mMapFAB.setOnClickListener {
+            val tabLayout = activity!!.findViewById(R.id.tab_layout) as TabLayout
+            tabLayout.getTabAt(1)!!.select()
+        }
 
         return rootView
     }
