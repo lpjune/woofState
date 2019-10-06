ae86d3fd87a9862925f8c57c9eb4761d6a8be158
Claudia <claudiaareneee@gmail.com>
2019-10-06 11:20:20 -0500

    WIP I'm probably going to delete these but I'm learning http requests

--- app/src/main/java/com/example/hackgsu19/Feed/FeedFragment.kt
+++ app/src/main/java/com/example/hackgsu19/Feed/FeedFragment.kt
@@ -6,10 +6,17 @@ import androidx.recyclerview.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import com.example.hackgsu19.R
 
 import 	androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.hackgsu19.DataModel
 import com.example.hackgsu19.MainActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
@@ -20,7 +27,7 @@ import kotlinx.android.synthetic.main.tab_layout.view.*
 class FeedFragment: Fragment() {
 
     private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>? = null
+    private var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
 
     companion object {
         fun newInstance(): FeedFragment {
@@ -40,6 +47,8 @@ class FeedFragment: Fragment() {
         val mMapFAB = rootView.findViewById(R.id.floating_action_button_map) as FloatingActionButton
         mMapFAB.setOnClickListener {
             (activity as MainActivity).switchFragment(1)
+            val dataModel = DataModel()
+            dataModel.sendRequest(context!!,activity as FragmentActivity)
         }
 
         return rootView
